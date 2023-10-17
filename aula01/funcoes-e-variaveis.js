e37882cc56786564397bb5ed8fbb5f9959e1b63a
fabioantunes2806 <fabioantunes2806@gmail.com>
2023-10-16 22:25:50 -0300

    Primeira aula de JavaScript

---
+++ aula01/funcoes-e-variaveis.js
@@ -0,0 +1,32 @@
+var variavel1 = "olá";
+let variavel2 = 3;
+const variavel3 = 8;
+/*
+console.log("Olá, mundo!");
+console.log(variavel1 + "mundo");
+console.log(variavel1, "mundo");
+
+console.log(variavel1 + " Turminha do " + variavel2 + "º ano");
+
+console.log(variavel2 + variavel3);
+console.log(variavel2 - variavel3);
+console.log(variavel2 * variavel3);
+console.log(variavel2 / variavel3);
+
+console.log(typeof variavel1);
+console.log(typeof variavel2); */
+
+const pessoa = {
+    nome: "Fábio",
+    signo: "Câncer",
+    idade: 18, 
+}
+
+console.log(pessoa);
+console.log(
+    "Nome:", pessoa.nome,
+    "\nIdade:", pessoa.idade   
+);
+
+let comidas = ["Pizza", "Hambúrguer", "Batata frita"];
+console.log(comidas[1])
